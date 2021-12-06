09ce20b1ea23f57428ceae19731c8cca6eef2ca3
Danny10ison <pagesngod@gmail.com>
2021-12-06 08:08:26 +0000

    Passing Data from nested component using @Output

--- APM-Start/src/app/shared/star.component.ts
+++ APM-Start/src/app/shared/star.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges } from "@angular/core";
+import { Component, EventEmitter, Input, OnChanges, Output } from "@angular/core";
 
 
 @Component({
@@ -10,12 +10,13 @@ import { Component, Input, OnChanges } from "@angular/core";
 export class StarComponent implements OnChanges{
     @Input() rating: number = 0;
     cropWidth: number = 75;
+    @Output()ratingClicked: EventEmitter<string> = new EventEmitter<string>();
 
     ngOnChanges(): void{
         this.cropWidth = this.rating* 75/ 5;  // stars
     }
 
     onClick(): void{
-        console.log(`The rating ${this.rating} was clicked`);
+        this.ratingClicked.emit(`The rating ${this.rating} was clicked`);
     }
 }
